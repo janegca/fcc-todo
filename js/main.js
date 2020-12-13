7ce2de2eb0228aba89bf7bbfcd7f842d6b2741de
janegca <janegca@users.noreply.github.com>
2020-12-13 11:33:32 -0500

    added event listener and code to the Clear btn

--- js/main.js
+++ js/main.js
@@ -19,6 +19,21 @@ const initApp = () => {
     processSubmission();
   });
 
+  const clearItems = document.getElementById("clearItems");
+  clearItems.addEventListener("click", (ev) => {
+    const list = toDoList.getList();
+    if (list.length) {
+      const confirmed = confirm(
+        "Are you sure you want to clear the entire list?"
+      );
+      if (confirmed) {
+        toDoList.clearList();
+        // TODO: update persistent data
+        refreshPage();
+      }
+    }
+  });
+
   // Procedural
   // load list object
 
